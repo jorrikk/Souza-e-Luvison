e8c47e75e171fa746f5d17d332d44b9a14658e31
jorrikk <65310745+jorrikk@users.noreply.github.com>
2020-05-31 03:21:21 -0300

    Add files via upload
    ver 2.0.4 beta

--- main.js
+++ main.js
@@ -3,57 +3,26 @@
     $body.classList.remove('no-js')
     $body.classList.add('js')
 
-    var $btnMenu = document.querySelector('.btn-js')
-
-    var $btnMenu2 = document.querySelector('.btn-js2')
-    
-    var $menu_js = document.querySelector('.menu-showed')
+    menujs()
 
 
-    var $menu = document.querySelector('.menu')
+  
 
-
-    var $hmbgr_menu = document.querySelector('.menu-js')
-    
-    
-    
-    $btnMenu.addEventListener('click', function(e){
-        togglehidden(e.target)
-
-        
-        
-        
-    })
-
-    function togglehidden(){
-        $menu_js.classList.toggle('hidden')
-    }
-    
-
-    
-    window.addEventListener('scroll', function(){
-      scrollcheck()  
-    })
-    
-    function scrollcheck(){
-       var windowTop = window.pageYOffset 
-       if (windowTop < 200) {
-           $hmbgr_menu.classList.add('js-hidden')
-           $menu_js.classList.add('hidden')
-        }   else {
-               $hmbgr_menu.classList.remove('js-hidden')
-           }
-       
-    }
-
-
-    var caroulImgs = new Carousel ({
+    var carouselImgs = new Carousel ({
         container: '.especialidades',
         itens: '.card',
         btnPrev: '.prev',
         btnNext: '.next'
 
     })
+
+
+
+
+
+   
+
+    
 
 
 
--- menu.js
+++ menu.js
@@ -1,2 +1,46 @@
 
 
+function menujs () {
+    var $btnMenu = document.querySelector('.btn-js')
+
+    var $btnMenu2 = document.querySelector('.btn-js2')
+    
+    var $menu_js = document.querySelector('.menu-showed')
+
+
+    var $menu = document.querySelector('.menu')
+
+
+    var $hmbgr_menu = document.querySelector('.menu-js')
+    
+    
+    
+    $btnMenu.addEventListener('click', function(e){
+        togglehidden(e.target)
+
+        
+        
+        
+    })
+
+    function togglehidden(){
+        $menu_js.classList.toggle('hidden')
+    }
+    
+
+    
+    window.addEventListener('scroll', function(){
+      scrollcheck()  
+    })
+    
+    function scrollcheck(){
+       var windowTop = window.pageYOffset 
+       if (windowTop < 200) {
+           $hmbgr_menu.classList.add('js-hidden')
+           $menu_js.classList.add('hidden')
+        }   else {
+               $hmbgr_menu.classList.remove('js-hidden')
+           }
+       
+    }
+}
